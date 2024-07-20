004e8e7cc8c72119d04fd06c7e2de476b66e0a04
timaroblox5 <timaroblox9@gmail.com>
2024-07-20 20:57:55 +0300

    z

--- index.js
+++ index.js
@@ -1,10 +1,9 @@
-// Index.js
 const { Client, GatewayIntentBits } = require('discord.js');
 
 // Создаем клиент с необходимыми интентами
-const client = new Client({intents: [32000] });
+const client = new Client({ intents: [GatewayIntentBits.All] });
 
-const TOKEN = process.env.DISCORD_TOKEN; // Берем токен из переменной окружения
+const TOKEN = process.env.TOKEN; // Берем токен из переменной окружения
 
 client.once('ready', () => {
     console.log('Bot is online!');
