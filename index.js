cbea1bfe6cf528cdee715018db462811d55e362a
timaroblox5 <timaroblox9@gmail.com>
2024-07-20 22:21:59 +0300

    upd

--- index.js
+++ index.js
@@ -1,6 +1,5 @@
 const { Client, GatewayIntentBits } = require('discord.js');
-const sqlite3 = require('sqlite3').verbose();
-const db = new sqlite3.Database('./pd-db/pinoBotDatabase.db');
+const db = require('./pb-db/index');
 
 const client = new Client({ 
     intents: [
@@ -10,23 +9,10 @@ const client = new Client({
     ],
 });
 
-db.serialize(() => {
-    db.run("CREATE TABLE IF NOT EXISTS users (id INTEGER PRIMARY KEY, name TEXT)");
-});
-
 // Логин бота
 client.once('ready', () => {
     console.log('PinoBot is online!');
 
-    // Пример добавления пользователя (на ваше усмотрение)
-    const stmt = db.prepare("INSERT INTO users (name) VALUES (?)");
-    stmt.run("Pino");
-    stmt.finalize();
-
-    // Пример чтения пользователей
-    db.each("SELECT id, name FROM users", (err, row) => {
-        console.log(row.id + ": " + row.name);
-    });
 });
 
 const TOKEN = process.env.DISCORD_TOKEN; // Получаем токен из переменной окружения
