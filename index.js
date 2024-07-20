b9018727bb94c5fee1c86a90006c2a50be82302b
timaroblox5 <timaroblox9@gmail.com>
2024-07-20 22:25:49 +0300

    upd2

--- index.js
+++ index.js
@@ -1,7 +1,8 @@
 const { Client, GatewayIntentBits } = require('discord.js');
-const db = require('./pb-db/index');
+const fs = require('fs');
+const path = './pd-db/users.json'; // Убедитесь, что путь правильный
 
-const client = new Client({ 
+const client = new Client({
     intents: [
         GatewayIntentBits.Guilds,
         GatewayIntentBits.GuildMessages,
@@ -9,12 +10,28 @@ const client = new Client({
     ],
 });
 
-// Логин бота
-client.once('ready', () => {
-    console.log('PinoBot is online!');
+// Убедитесь, что файл данных существует
+let users = [];
+if (!fs.existsSync(path)) {
+    fs.writeFileSync(path, JSON.stringify([])); // Создание пустого массива, если файла нет
+}
+const rawData = fs.readFileSync(path);
+users = JSON.parse(rawData);
 
+client.once('ready', () => {
+    console.log('PunoBot is online!');
+
+    // Пример добавления пользователя
+    users.push({ id: users.length + 1, name: 'Puno' });
+
+    // Сохранение данных обратно в JSON-файл
+    fs.writeFileSync(path, JSON.stringify(users, null, 2));
+
+    // Чтение и вывод данных
+    users.forEach(user => {
+        console.log(user.id + ": " + user.name);
+    });
 });
 
 const TOKEN = process.env.DISCORD_TOKEN; // Получаем токен из переменной окружения
-// Логинимся с токеном
 client.login(TOKEN);
