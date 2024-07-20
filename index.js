2b08972a06ae2435618513bad5f98cefd6e2bc89
timaroblox5 <timaroblox9@gmail.com>
2024-07-21 00:32:59 +0300

    Update index.js

--- index.js
+++ index.js
@@ -12,40 +12,5 @@ const client = new Client({
 client.once('ready', () => {
     console.log('PunoBot is online!');
 });
-
-client.on('messageCreate', async (message) => {
-    if (message.author.bot) return;
-
-    const args = message.content.trim().split(' ');
-
-    // Команда для добавления пользователя
-    if (args[0] === '!adduser' && args[1]) {
-        const newUser = new User({ id: Date.now(), name: args[1] });
-
-        try {
-            await newUser.save();
-            message.channel.send(`Пользователь ${args[1]} добавлен!`);
-        } catch (error) {
-            console.error('Ошибка при сохранении пользователя:', error);
-            message.channel.send('Ошибка при добавлении пользователя.');
-        }
-    }
-
-    // Команда для вывода всех пользователей
-    else if (args[0] === '!listusers') {
-        try {
-            const users = await User.find(); // Получение всех пользователей из базы данных
-            if (users.length === 0) {
-                message.channel.send('Нет пользователей.');
-            } else {
-                const userList = users.map(user => `${user.id}: ${user.name}`).join('\n');
-                message.channel.send(`Список пользователей:\n${userList}`);
-            }
-        } catch (error) {
-            console.error('Ошибка при получении пользователей:', error);
-            message.channel.send('Ошибка при получении списка пользователей.');
-        }
-    }
-});
 const TOKEN = process.env.DISCORD_TOKEN; // Получаем токен из переменной окружения
 client.login(TOKEN);
