f6354a26c4faebe67ca609dfd931a243a042f0a6
timaroblox5 <timaroblox9@gmail.com>
2024-07-21 10:46:58 +0300

    Update index.js

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-const { Client, GatewayIntentBits } = require('discord.js');
+const { Client, GatewayIntentBits, Collection } = require('discord.js'); // Добавьте Collection
 const fs = require('fs');
 const path = require('path');
 const User = require('./pd-db/userModel'); // Импортируйте модель пользователя
@@ -17,7 +17,7 @@ client.once('ready', () => {
 });
 
 // Загружаем команды
-client.commands = new Discord.Collection();
+client.commands = new Collection(); // Используйте импортированную Collection
 
 const commandFiles = fs.readdirSync(path.join(__dirname, 'commands')).filter(file => file.endsWith('.js'));
 
@@ -26,40 +26,38 @@ for (const file of commandFiles) {
     client.commands.set(command.name, command);
 }
 
-// Функция для загрузки обработчиков событий
+// Загрузка обработчиков и событий
 const loadEvents = (client) => {
     const eventFiles = fs.readdirSync(path.join(__dirname, 'events')).filter(file => file.endsWith('.js'));
 
     for (const file of eventFiles) {
         const event = require(`./events/${file}`);
         const eventName = file.split('.')[0];
-        client.on(eventName, event.bind(null, client)); // Передаем client в обработчик
+        client.on(eventName, event.bind(null, client));
     }
 };
 
-// Функция для загрузки обработчиков
 const loadHandlers = (client) => {
     const handlerFiles = fs.readdirSync(path.join(__dirname, 'handlers')).filter(file => file.endsWith('.js'));
 
     for (const file of handlerFiles) {
         const handler = require(`./handlers/${file}`);
         const handlerName = file.split('.')[0];
-        client.on(handlerName, handler.bind(null, client)); // Передаем client в обработчик
+        client.on(handlerName, handler.bind(null, client));
     }
 };
 
-// Функция для загрузки структур (если вы это планируете)
 const loadStructures = (client) => {
     const structureFiles = fs.readdirSync(path.join(__dirname, 'structures')).filter(file => file.endsWith('.js'));
 
     for (const file of structureFiles) {
         const structure = require(`./structures/${file}`);
         const structureName = file.split('.')[0];
-        client.on(structureName, structure.bind(null, client)); // Передаем client в обработчик
+        client.on(structureName, structure.bind(null, client));
     }
 };
 
-// Загрузка событий, обработчиков и структур
+// Загружаем обработчики и структуры
 loadEvents(client);
 loadHandlers(client);
 loadStructures(client);
