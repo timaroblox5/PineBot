e6172b14c9807a73e184c51c4a64434d0125aaf7
timaroblox5 <timaroblox9@gmail.com>
2024-07-21 11:26:06 +0300

    Update index.js

--- index.js
+++ index.js
@@ -1,7 +1,5 @@
 const { Client, GatewayIntentBits, Collection } = require('discord.js');
 const fs = require('fs');
-const commands = require('./handlers/commands.js');
-const events = require('./handlers/events.js');
 const path = require('path');
 const Config = require('./config.json');
 const client = new Client({
