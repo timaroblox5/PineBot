2c6a7cc49daa5d546cd077976de03f3e24a6f51f
timaroblox5 <timaroblox9@gmail.com>
2024-07-22 12:58:55 +0300

    Update ready.js

--- src/events/ready.js
+++ src/events/ready.js
@@ -3,5 +3,14 @@ module.exports = {
     once: true, // Событие будет вызвано только один раз
     execute(client) {
         console.log(`Бот ${client.user.tag} готов к работе!`);
+        const CHANNEL_ID = '1264719358563979325';
+         // Находим канал по ID
+    const channel = client.channels.cache.get(CHANNEL_ID);
+    if (channel) {
+        // Отправляем сообщение
+        channel.send('Привет, мир! Это сообщение от бота.').catch(console.error);
+    } else {
+        console.log('Канал не найден');
+    }
     },
 };
