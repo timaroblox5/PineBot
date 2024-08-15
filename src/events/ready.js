daa0209b1e7f1ff1b02d240d51991b96586119ea
timaroblox5 <timaroblox9@gmail.com>
2024-08-15 10:01:01 +0300

    z

--- src/events/ready.js
+++
@@ -1,7 +0,0 @@
-module.exports = {
-    name: 'ready',
-    once: true, // Событие будет вызвано только один раз
-    execute(client) {
-        console.log(`Бот ${client.user.tag} готов к работе!`);
-    },
-};
