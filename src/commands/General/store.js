af40af2445bf3bf461960e6bdee69498d663555a
timaroblox5 <timaroblox9@gmail.com>
2024-08-19 18:38:00 +0300

    z

---
+++ src/commands/General/store.js
@@ -0,0 +1,10 @@
+const { MessageEmbed } = require('discord.js');
+const config = require('../../../config.json');
+
+module.exports = {
+    data: {
+        name: 'store',
+        description: '',
+        usage: '[category]',
+    },
+}
